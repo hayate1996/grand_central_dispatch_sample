c0a41bcf78c15a0a391109dd47f7c088e3939cb8
NAGAMINE HIROMASA <JP11762@w010d71512009m.local>
2016-07-15 17:34:41 +0900

    Initial Commit

---
+++ gcd_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gcd_sample
+//
+//  Created by NAGAMINE HIROMASA on 2016/07/15.
+//  Copyright © 2016年 NAGAMINE HIROMASA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
